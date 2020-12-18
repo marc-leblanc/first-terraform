c587c9972b239d1d771df68cf91f0f4941e3ad35
Marc LeBlanc <marc.leblanc@arctiq.ca>
2020-12-18 14:55:01 +0000

    first terraform project

---
+++ main.tf
@@ -0,0 +1,17 @@
+
+resource "google_compute_instance" "default" {
+  name         = "my-first-instance"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+}
